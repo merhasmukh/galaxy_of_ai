55fc9235def8e63d986bf875d96dc213b12f0979
merhasmukh <mhgn0001@gmail.com>
2024-12-25 11:24:14 +0530

    ads added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 
 import "./globals.css";
 
-
-
+import Head from "next/head";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -11,6 +11,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1479377630872521"/>
+         <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-84FYVM6X8L');
+            `}
+          </Script>
+        </Head>
       <body>
         {children}
       </body>
