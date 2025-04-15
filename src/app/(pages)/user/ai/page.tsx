f69f53ab452849a50956ce7e2fa27feaca7348fb
merhasmukh <mhgn0001@gmail.com>
2025-04-15 21:35:22 +0530

    enhance chatbot code

--- src/app/(pages)/user/ai/page.tsx
+++ src/app/(pages)/user/ai/page.tsx
@@ -1,11 +1,31 @@
 import UserAI from "@/app/components/User/AI/UserAI";
 import { cookies } from "next/headers";
-
+import { redirect } from "next/navigation";
 
 export default async function UserAIage (){
     const cookieStore = await cookies();       // ✅ CORRECT
     const accessToken = cookieStore.get("accessToken")?.value;
 
+    if (!accessToken) {
+            redirect("/user/login");
+        }
+    
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BE_API_URL}/auth/validate_token/`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            cache: "no-store",
+        });
+    
+        if (!res.ok) {
+            redirect("/user/login");
+        }
+    
+        const data = await res.json();
+    
+        if (!data.valid) {
+            redirect("/user/login");
+        }
 
 
     return <UserAI accessToken={accessToken}/>
