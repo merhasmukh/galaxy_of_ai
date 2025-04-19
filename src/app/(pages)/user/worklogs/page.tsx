6e01e2e9c6d510c111e7176711b93fd7a9949ea9
merhasmukh <mhgn0001@gmail.com>
2025-04-19 15:29:30 +0530

    add user validation on apis

--- src/app/(pages)/user/worklogs/page.tsx
+++ src/app/(pages)/user/worklogs/page.tsx
@@ -1,9 +1,32 @@
 import WorkLogManager from "@/app/components/User/WorkLog/WorkLog";
 
-
-const page = () =>{
-    return <WorkLogManager />
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export default async function WorkLogPage(){
+        const cookieStore = await cookies();       // ✅ CORRECT
+        const accessToken = cookieStore.get("accessToken")?.value;
+        
+            if (!accessToken) {
+                    redirect("/user/login");
+                }
+            
+                const res = await fetch(`${process.env.NEXT_PUBLIC_BE_API_URL}/auth/validate_token/`, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                    cache: "no-store",
+                });
+            
+                if (!res.ok) {
+                    redirect("/user/login");
+                }
+            
+                const data = await res.json();
+            
+                if (!data.valid) {
+                    redirect("/user/login");
+                }
+    return <WorkLogManager accessToken={accessToken}/>
      
 };
 
-export default page;
